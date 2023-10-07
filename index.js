bd87259cd19d1de3a917d026c581a210947890a2
Adit Shetty <adshetty1997@gmail.com>
2023-10-07 21:56:14 +0530

    sandbox options

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ app.post("/update-profile-pic",async(req,res)=>{
 
 const currentClient = new Client({
     authStrategy:new LocalAuth(),
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
 });
 
 currentClient.on('qr', (qrcode) => {
